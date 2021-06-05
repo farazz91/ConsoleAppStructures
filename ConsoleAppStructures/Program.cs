2781985df3fc219826757b4391f6ecfa1420d1a8
farazz91 <farazz91@gmail.com>
2021-06-05 16:44:13 +0530

    Add project files.

---
+++ ConsoleAppStructures/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsoleAppStructures
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
