9a5b1d721daef733c7af99a7be778a369db069c2
farazz91 <farazz91@gmail.com>
2021-06-05 16:54:43 +0530

    Structures demo

--- ConsoleAppStructures/Program.cs
+++ ConsoleAppStructures/Program.cs
@@ -6,7 +6,18 @@ namespace ConsoleAppStructures
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Customer cust1 = new Customer(101, "Sarfaraz");
+            cust1.printCustomerDetails();
+            Console.WriteLine();
+
+            Customer cust2 = new Customer();
+            cust2.ID = 102;
+            cust2.Name = "Faraz";
+            cust2.printCustomerDetails();
+            Console.WriteLine();
+
+            Customer cust3 = new Customer() { ID = 103, Name = "Sarf" };
+            cust3.printCustomerDetails();
         }
     }
 }
